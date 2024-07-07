cd4815c73473427a977778718526dea23cecbd5d
yuji <yujisakuma224@gmail.com>
2024-07-07 15:44:32 -0300

    adicionando projeto de backend com Spring

---
+++ backend/gerenciadoDoacaoIMDAZ/src/main/java/br/com/ucpel/pi3/b/gerenciadoDoacaoIMDAZ/GerenciadoDoacaoImdazApplication.java
@@ -0,0 +1,13 @@
+package br.com.ucpel.pi3.b.gerenciadoDoacaoIMDAZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadoDoacaoImdazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadoDoacaoImdazApplication.class, args);
+	}
+
+}
---
+++ backend/gerenciadoDoacaoIMDAZ/src/test/java/br/com/ucpel/pi3/b/gerenciadoDoacaoIMDAZ/GerenciadoDoacaoImdazApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucpel.pi3.b.gerenciadoDoacaoIMDAZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadoDoacaoImdazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
