9b8ad5c1d66fca2e8ee9613602cadac2639de335
PedroHEBR <pedrohtalt@outlook.com>
2024-07-08 19:52:08 -0300

    Add files via upload

---
+++ modelos/Cobrancas.java
@@ -0,0 +1,25 @@
+package modelos;
+
+import java.util.Date;
+
+public class Cobrancas {
+    private Long id;
+    private Date vencimento;
+    private Date parcela;
+
+    public Date getParcela() {
+        return parcela;
+    }
+
+    public void setParcela(Date parcela) {
+        this.parcela = parcela;
+    }
+
+    public Date getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(Date vencimento) {
+        this.vencimento = vencimento;
+    }
+}
