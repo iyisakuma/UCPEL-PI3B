6b4cd5f3c9a3a51ca678e56900d110319479f0ff
yuji <yujisakuma224@gmail.com>
2024-07-12 21:58:31 -0300

    Criacao de entidade JPA e relacionamento entre eles

--- backend/gerenciadoDoacaoIMDAZ/src/main/java/br/com/ucpel/pi3/b/gerenciadoDoacaoIMDAZ/domain/PessoaFisica.java
+++ backend/gerenciadoDoacaoIMDAZ/src/main/java/br/com/ucpel/pi3/b/gerenciadoDoacaoIMDAZ/domain/PessoaFisica.java
@@ -1,10 +1,12 @@
 package br.com.ucpel.pi3.b.gerenciadoDoacaoIMDAZ.domain;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Entity
 public class PessoaFisica extends Pessoa{
     private String cpf;
     private LocalDate dtNascimento;
--- backend/gerenciadoDoacaoIMDAZ/src/main/java/br/com/ucpel/pi3/b/gerenciadoDoacaoIMDAZ/domain/PessoaJuridica.java
+++ backend/gerenciadoDoacaoIMDAZ/src/main/java/br/com/ucpel/pi3/b/gerenciadoDoacaoIMDAZ/domain/PessoaJuridica.java
@@ -1,10 +1,12 @@
 package br.com.ucpel.pi3.b.gerenciadoDoacaoIMDAZ.domain;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Entity
 public class PessoaJuridica extends Pessoa {
     private String cnpj;
     private Date dtFundacao;
